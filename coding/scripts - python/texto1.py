e2ed7fa2e2ae8cc422a4f53571b951d509fb4dc1
Ex-Zos <app.juliog@gmail.com>
2023-05-11 17:55:28 -0300

    Correction

--- coding/scripts - python/texto1.py
+++
@@ -1,6 +0,0 @@
-dia = input('Que dia você nasceu? ')
-mes = input('Que mês? ')
-ano = input('Que ano? '), int
-nascimento = (dia + '/' + mes + '/' + ano)
-print('Você nasceu em ', nascimento, ', correto? ')
-print('Você tem ', 2022-(ano), 'anos.')
